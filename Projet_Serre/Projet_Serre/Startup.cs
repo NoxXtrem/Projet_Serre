f84f299efacee76310cc2a1f80a96cfc5f2b72b4
john.vanderlick <john.vanderlick@gmail.com>
2016-01-25 16:42:16 +0100

    azerty

---
+++ Projet_Serre/Projet_Serre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projet_Serre.Startup))]
+namespace Projet_Serre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
