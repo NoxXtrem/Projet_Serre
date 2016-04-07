d2a7449712722bcf3b91fa683855dcb9c2550806
wobah <julesdu07@laposte.net>
2016-04-07 15:35:24 +0200

    ajout du code relever valeur phidgets

--- Projet_Serre/Projet_Serre/Models/GestionCapteur.cs
+++ Projet_Serre/Projet_Serre/Models/GestionCapteur.cs
@@ -1,4 +1,7 @@
 using System;
+using Phidgets;
+using Phidgets.Events;
+
 public class GestionCapteur {
 	private Capteur capteurEnso;
 	public Capteur CapteurEnso {
@@ -26,6 +29,21 @@ public class GestionCapteur {
 	}
 
 	public GestionCapteur() {
+
+        // Ajouter code démarage phidgets
+
+        InterfaceKit ifKit;
+        ifKit = new InterfaceKit();
+        //Hook the basic event handlers
+        ifKit.Attach += new AttachEventHandler(ifKit_Attach);
+        ifKit.Detach += new DetachEventHandler(ifKit_Detach);
+        ifKit.Error += new ErrorEventHandler(ifKit_Error);
+        //Hook thephidget specific event handlers
+        ifKit.SensorChange += new SensorChangeEventHandler(ifKit_SensorChange);
+        ifKit.open();
+ 
+
+        //
         capteurEnso = new CapteurEnso();
         anemometre = new Anemometre();
         capteurTemperature = new CapteurTemp();
@@ -34,4 +52,32 @@ public class GestionCapteur {
 
 	private Capteur[] capteur;
 
+
+    static void ifKit_Attach(object sender, AttachEventArgs e)
+    {
+        Console.WriteLine("InterfaceKit {0} attached!",
+                            e.Device.SerialNumber.ToString());
+    }
+
+    //Detach event handler...Display the serial number of the detached InterfaceKit 
+    //to the console
+    static void ifKit_Detach(object sender, DetachEventArgs e)
+    {
+        Console.WriteLine("InterfaceKit {0} detached!",
+                            e.Device.SerialNumber.ToString());
+    }
+
+    //Error event handler...Display the error description to the console
+    static void ifKit_Error(object sender, ErrorEventArgs e)
+    {
+        Console.WriteLine(e.Description);
+    }
+
+    //Sensor Change event handler...Display the sensor index and it's new value to 
+    //the console
+    static void ifKit_SensorChange(object sender, SensorChangeEventArgs e)
+    {
+        Console.WriteLine("Sensor index {0} value {1}", e.Index, e.Value);
+
+    }
 }
